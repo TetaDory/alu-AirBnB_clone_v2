78c3bdf7eba017b677f9abb9fbf8a1ad9ebde188
TetaDory <d.rutayisir1@alustudent.com>
2023-06-22 06:23:52 -0700

    c

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+ """
+    Generates a .tgz archive from the contents of the web_static folder.
+
+    Returns:
+        str: The path of the generated archive if successful, None otherwise.
+"""
+
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_path = "versions/web_static_{}.tgz".format(now)
+
+      # Use 'tar' command to create the .tgz archive
+    result = local("tar -czvf {} web_static".format(archive_path))
+
+    if result.failed:
+        return None
+
+    return archive_path
