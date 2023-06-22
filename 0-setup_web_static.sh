26e4c526a2731659cc1dfbd86649101b94cbc592
TetaDory <113389069+TetaDory@users.noreply.github.com>
2023-06-22 16:07:50 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,28 +1,17 @@
 #!/usr/bin/env bash
 # This Bash script sets up web servers for the deployment of web_static.
 
-if ! dpkg -s nginx &> /dev/null; then
-    apt-get -y update
-    apt-get -y install nginx
-fi
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo service nginx start
 
-mkdir -p /data/web_static/shared
-mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "<head></head>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-
-rm -rf /data/web_static/current
-ln -s /data/web_static/releases/test /data/web_static/current
-
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
-systemctl restart nginx
-exit 0
+sudo service nginx restart
