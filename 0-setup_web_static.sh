77899abddd84aa213368f764bac361771a35f282
TetaDory <113389069+TetaDory@users.noreply.github.com>
2023-06-22 13:21:10 +0300

    c

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,45 +1,40 @@
 #!/usr/bin/env bash
-#This Bash script sets up web servers for the deployment of web_static.
+
+# This Bash script sets up web servers for the deployment of web_static.
 
 # Install Nginx if not already installed
-
 if ! dpkg -s nginx &> /dev/null; then
     apt-get -y update
     apt-get -y install nginx
 fi
 
-#create folders
+# Create folders
+mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/releases/test
 
-mkdir -p data/web_static/shared
-mkdir -p data/web_static/releases/test
-
-#create fake html file
-
-echo"<html>
+# Create fake HTML file
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > data/web_static/releases/test/index.html
+</html>" > /data/web_static/releases/test/index.html
 
-#Create a symbolic link
-
+# Create a symbolic link
 rm -f /data/web_static/current
 ln -s /data/web_static/releases/test/ /data/web_static/current
 
-#Give ownership
-
+# Give ownership
 chown -R ubuntu:ubuntu /data/
 
-#Update the Nginx configuration
-
+# Update the Nginx configuration
 config_file="/etc/nginx/sites-available/default"
 config_text="location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
 sed -i "/server {/a $config_text" $config_file
 
-#Restart Nginx
-
+# Restart Nginx
 service nginx restart
 
+# Exit with success code
 exit 0
