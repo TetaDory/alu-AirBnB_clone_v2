ca3eccb64c72adeb04b3befcf40746c247eba98e
TetaDory <d.rutayisir1@alustudent.com>
2023-06-22 03:14:53 -0700

    c

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
-# This Bash script sets up web servers for the deployment of web_static.
+#This Bash script sets up web servers for the deployment of web_static.
 
 # Install Nginx if not already installed
 
